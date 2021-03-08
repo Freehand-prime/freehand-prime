cc761171ae4c6c0cd6eb89ce04e442410b480206
NateMacLaurin <brow1982@umn.edu>
2021-03-08 16:01:52 -0600

    added button and redirect to person

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -2,21 +2,18 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import './LandingPage.css';
 
-// CUSTOM COMPONENTS
-import RegisterForm from '../RegisterForm/RegisterForm';
-
 export default function LandingPage() {
-  const [heading, setHeading] = useState('Welcome');
   const history = useHistory();
 
-  const onLogin = (event) => {
-    history.push('/login');
+  const enterPersonRedirect = () => {
+      //debug
+    console.log('enterPersonRedirect: Clicked "Who do you Appreciate" on Landing Page - Directing to EnterPerson.')
+      //redirect
+    history.push('/person');
   };
 
   return (
     <div className="container">
-      <h2>{heading}</h2>
-
       <div className="grid">
         <div className="grid-col grid-col_8">
           <p>
@@ -30,34 +27,11 @@ export default function LandingPage() {
             Quisque eget eros metus. Vestibulum bibendum fringilla nibh a
             luctus. Duis a sapien metus.
           </p>
-
-          <p>
-            Praesent consectetur orci dui, id elementum eros facilisis id. Sed
-            id dolor in augue porttitor faucibus eget sit amet ante. Nunc
-            consectetur placerat pharetra. Aenean gravida ex ut erat commodo, ut
-            finibus metus facilisis. Nullam eget lectus non urna rhoncus
-            accumsan quis id massa. Curabitur sit amet dolor nisl. Proin
-            euismod, augue at condimentum rhoncus, massa lorem semper lacus, sed
-            lobortis augue mi vel felis. Duis ultrices sapien at est convallis
-            congue.
-          </p>
-
-          <p>
-            Fusce porta diam ac tortor elementum, ut imperdiet metus volutpat.
-            Suspendisse posuere dapibus maximus. Aliquam vitae felis libero. In
-            vehicula sapien at semper ultrices. Vivamus sed feugiat libero. Sed
-            sagittis neque id diam euismod, ut egestas felis ultricies. Nullam
-            non fermentum mauris. Sed in enim ac turpis faucibus pretium in sit
-            amet nisi.
-          </p>
         </div>
         <div className="grid-col grid-col_4">
-          <RegisterForm />
-
           <center>
-            <h4>Already a Member?</h4>
-            <button className="btn btn_sizeSm" onClick={onLogin}>
-              Login
+            <button className="btn btn_sizeSm" onClick={enterPersonRedirect}>
+              Who Do You Appreciate?
             </button>
           </center>
         </div>
