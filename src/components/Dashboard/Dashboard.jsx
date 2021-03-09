dafba64b4af6da0e2b07a7ae7c408a8844ed718e
Sean Wade <smwade1115@gmail.com>
2021-03-08 19:55:41 -0600

    dashboard front end with dummy data built

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,5 +1,83 @@
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Card,
+  CardActions,
+  CardContent,
+  Container,
+  Grid,
+  Paper,
+  Typography,
+} from '@material-ui/core';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
 export default function Dashboard() {
-    return (
-        <h1>Dashboard</h1>
-    )
+  const history = useHistory();
+  const classes = useStyles();
+  // const events = useSelector((store) => store.events);
+
+  // do we not need a useState bc we are setting state after registration / login?
+  const dummyEvent = {
+    date: '3/14/2021',
+    person: 'Brian',
+    occasion: 'birthday',
+    category: 'memes',
+    card_id: null,
+  };
+
+  return (
+    <>
+      <Container>
+        <br></br>
+        <Typography variant="h5">Dashboard</Typography>
+        <br></br>
+        <Paper>
+          <Card className={classes.root}>
+            <CardContent>
+              <Typography
+                className={classes.title}
+                color="textSecondary"
+                gutterBottom
+              >
+                Next Event - {dummyEvent.date}
+              </Typography>
+              <Typography variant="h5" component="h2">
+                {dummyEvent.person}
+              </Typography>
+              <Typography className={classes.pos} color="textSecondary">
+                {dummyEvent.occasion} | {dummyEvent.category}
+              </Typography>
+              <Button size="small">Edit Event</Button>
+            </CardContent>
+          </Card>
+        </Paper>
+        <br></br>
+        <Box justifyContent="center">
+          <Paper>
+            <ButtonGroup orientation="vertical" color="primary" variant="text">
+              <Button>Who Do You Appreciate?</Button>
+              <Button>View People</Button>
+              <Button>View Calendar</Button>
+            </ButtonGroup>
+          </Paper>
+        </Box>
+      </Container>
+    </>
+  );
 }
