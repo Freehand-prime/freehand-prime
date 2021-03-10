e9ba2e4401d7363354f308e703240795d0f0a2f6
NateMacLaurin <brow1982@umn.edu>
2021-03-10 15:16:58 -0600

    added initial formcontrol and base styling for /shipping

--- src/components/ShippingConfirm/ShippingConfirm.jsx
+++ src/components/ShippingConfirm/ShippingConfirm.jsx
@@ -8,9 +8,13 @@ import {
     makeStyles,
     InputLabel,
     FormControl,
+    FormLabel,
+    FormControlLabel,
     Select,
     Paper,
     Button,
+    Radio,
+    RadioGroup
 } from '@material-ui/core';
 
 
@@ -31,11 +35,64 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function ShippingConfirm() {
+      //hooks
     const classes = useStyles(); 
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const selectedCard = useSelector((store) => store);
+
+      //handler functions
+    const handleChange = () => {
+      console.log('In handleChange');
+    }
+    const handleBack = () => {
+      console.log('In handleBack');
+    }
+    const handleSubmit = () => {
+      console.log('In handleSubmit');
+    }
 
     return (
-        <div className={classes.root}>
-            <h1>ShippingConfirm</h1>
-        </div>
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={6} sm={3}></Grid>
+            <Grid item xs={12} sm={6}>
+              <Paper align="center" elevation={4} className={classes.paper}>
+                <Typography variant="h5">Does Everything Look Correct?</Typography>
+              </Paper>
+            </Grid>
+            <Grid item xs={6} sm={3}></Grid>
+            <Grid item xs={6} sm={3}></Grid>
+            <Grid align="center" item xs={12} sm={6}>
+              <Paper elevation={4}>
+              <FormControl>
+                <FormLabel component="legend">Ship To:</FormLabel>
+                <RadioGroup aria-label="shipping" name="shipping1" value={"changeMe"} onChange={handleChange}>
+                  <FormControlLabel value="Ship to Them" control={<Radio />} label="Them" />
+                  <FormControlLabel value="Ship to Alternate Address" control={<Radio />} label="Alternate Address" />
+                    <TextField
+                      id="address_field"
+                      label= "address"
+                      placeholder="enter address"
+                      type="text"
+                      value={"changeMe"}
+                      onChange={(event) => dispatch({ type: 'SET_ADDRESS', payload: event.target.value })}
+                      variant="outlined"
+                    />
+                </RadioGroup>
+              </FormControl>
+                  <div>
+                      <Button variant="outlined" onClick={handleBack}>
+                          Back
+                      </Button>
+                      <Button variant="outlined" onClick={handleSubmit}>
+                          Submit
+                      </Button>
+                  </div>
+              </Paper>
+            </Grid>
+          <Grid item xs={6} sm={3}></Grid>
+        </Grid>
+      </div>
     )
 }
