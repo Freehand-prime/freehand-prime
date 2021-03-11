43809ff0e8e5daf9571f858f2f8a05a9d57066c3
Thomas Rohde <trohde@rocketmail.com>
2021-03-11 11:10:07 -0600

    created INSERT INTO data for cards and categories

---
+++ cards.sql
@@ -0,0 +1,12 @@
+
+
+INSERT INTO "cards" ("occasion_id", "category_id", "image_front", "image_inside", "artist", "details")
+VALUES (8, 5, 'https://freehand-prime.s3.us-east-2.amazonaws.com/card1front.jpeg', 'https://freehand-prime.s3.us-east-2.amazonaws.com/card1inside.jpeg', 'Wanda Maximoff', 'Pictoral scene that stresses solitude and taking time'),
+(1, 4, 'https://freehand-prime.s3.us-east-2.amazonaws.com/card2front.jpeg', 'https://freehand-prime.s3.us-east-2.amazonaws.com/card2inside.jpeg', 'Monica Rambeau', 'Sweet Fox, but you are wiser'),
+(9, 2, 'https://freehand-prime.s3.us-east-2.amazonaws.com/card3front.jpg', 'https://freehand-prime.s3.us-east-2.amazonaws.com/card3inside.png', 'Thor Odinson', 'Setting off on an adventure with you on a Viking ship'),
+(8, 6, 'https://freehand-prime.s3.us-east-2.amazonaws.com/card4front.jpg', 'https://freehand-prime.s3.us-east-2.amazonaws.com/card4inside.png', 'Darcy Lewis', 'Girl on a boat, dreaming'),
+(1, 3, 'https://freehand-prime.s3.us-east-2.amazonaws.com/card5front.jpg', 'https://freehand-prime.s3.us-east-2.amazonaws.com/card5inside.png', 'Clint Barton', 'Couple on a beach during sunset expressing togetherness'),
+(4, 2, 'https://freehand-prime.s3.us-east-2.amazonaws.com/card6front.jpg', 'https://freehand-prime.s3.us-east-2.amazonaws.com/card6inside.png', 'Natasha Romanoff', 'Girl stretching for a fight, dedicated and focused'),
+(11, 4, 'https://freehand-prime.s3.us-east-2.amazonaws.com/card7front.jpg', 'https://freehand-prime.s3.us-east-2.amazonaws.com/card7inside.png', 'Anthony Stark', 'Mother and children'),
+(9, 4, 'https://freehand-prime.s3.us-east-2.amazonaws.com/card8front.jpeg', 'https://freehand-prime.s3.us-east-2.amazonaws.com/card8inside.png', 'Pepper Potts', 'Elegant women in dresses'),
+(9, 9, 'https://freehand-prime.s3.us-east-2.amazonaws.com/card4front.jpg', 'https://freehand-prime.s3.us-east-2.amazonaws.com/card9inside.jpg', 'Carol Danvers', 'Superhero woman wants some quality time');
