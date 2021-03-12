6ebc582f57ad5bd485c08732069fef8ba10375ad
NateMacLaurin <blackmagenate@gmail.com>
2021-03-12 10:03:36 -0600

    added card pass on props to admincardtablerow

--- src/components/AdminCards/AdminCardTableRow/AdminCardTableRow.jsx
+++ src/components/AdminCards/AdminCardTableRow/AdminCardTableRow.jsx
@@ -7,16 +7,16 @@ import {
   TableCell
 } from '@material-ui/core';
 
-export default function AdminCardTableRow(card) {
+export default function AdminCardTableRow({card}) {
         //state
         //hooks
     const dispatch = useDispatch();
         //functions
     const handleEdit = () => {
-        console.log('handleEdit Clicked');
+        console.log('handleEdit Clicked on:', card.id);
     }
     const handleDelete = () => {
-        console.log('handleDelete Clicked');
+        console.log('handleDelete Clicked:', card.id);
     }
         //onRender
     return(
