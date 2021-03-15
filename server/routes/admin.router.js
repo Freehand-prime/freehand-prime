3c2e518a6e7e1bfdd038c7964fb7d438718c5b9d
NateMacLaurin <blackmagenate@gmail.com>
2021-03-15 02:12:08 -0500

    created delete route in admin router

--- server/routes/admin.router.js
+++ server/routes/admin.router.js
@@ -9,16 +9,64 @@ const {
  * adminCards routes for cards
  */
 
+//TODO: change rejectUnauthenticated middleware to handle rejectAdminUnauthenticated
+
 router.post('/card', rejectUnauthenticated, (req, res) => {
     // POST route code here
+        //store data on body in route scope
+    const newCardData = req.body;
+        //store query string in route scope
+    const cardToAddQuery = `
+
+        `;
+    try {
+
+    } catch(error) {
+        console.error(error);
+            //send response 500 'Internal Server Error' on pool query error 
+        res.sendStatus(500);
+    }
 });
 
 router.put('/card/', rejectUnauthenticated, (req, res) => {
     // POST route code here
+        //store data on body in route scope
+    const updateCardData = req.body;
+        //store query string in route scope
+    const cardToUpdateQuery = `
+    
+        `;
+    try {
+
+    } catch(error) {
+        console.error(error);
+            //send response 500 'Internal Server Error' on pool query error 
+        res.sendStatus(500);
+    }
 });
 
 router.delete('/card/:id', rejectUnauthenticated, (req, res) => {
     // POST route code here
+        //store ID in route scope
+    const cardToDeleteID = req.params.id;
+        //store query string in route scope
+    const cardToDeleteQuery = `
+        DELETE FROM "cards"
+        WHERE id=$1;
+        `;
+    pool
+        .query(cardToDeleteQuery, [
+        cardToDeleteID,
+        ])
+        .then(() => {
+                //send response 200 'OK' on pool query success 
+            res.sendStatus(200);
+        })
+        .catch((error) => {
+            console.error(error);
+                //send response 500 'Internal Server Error' on pool query error 
+            res.sendStatus(500);
+        });
 });
 
 /**
