e6ffe076ea65d8f692737b12d03d70a66e169db8
NateMacLaurin <blackmagenate@gmail.com>
2021-03-15 01:24:28 -0500

    deleteCard generator function created in cards.saga

--- src/redux/sagas/cards.saga.js
+++ src/redux/sagas/cards.saga.js
@@ -12,6 +12,37 @@ function* fetchCards() {
   }
 }
 
+function* addCard(action) {
+  console.log(`In addCard: ${action.payload}`);
+  try{
+
+  } catch(error) {
+    console.log(`ERROR in addCard: ${error}`);
+  }
+}
+
+function* editCard(action) {
+  console.log(`In editCard: ${action.payload}`);
+  try{
+
+  } catch(error) {
+    console.log(`ERROR in editCard: ${error}`);
+  }
+}
+
+function* deleteCard(action) {
+  console.log(`In deleteCard: ${action.payload}`);
+  try{
+      //store ID of card to delete
+    const cardId = action.payload;
+      //DELETE request to admin API
+    yield axios.delete(`/api/admin/${cardId}`);
+      //GET updated cards data.
+    yield put({type: 'FETCH_CARDS'});
+  } catch(error) {
+    console.log(`ERROR in deleteCard: ${error}`);
+  }
+}
 
 export default function* cardsSaga() {
   yield takeEvery("FETCH_CARDS", fetchCards);
