a2f5e289748f76bcdfe59a91e26235546710fcc2
NateMacLaurin <blackmagenate@gmail.com>
2021-03-15 03:38:51 -0500

    added edit.card.reducer.js to store card edit information

---
+++ src/redux/reducers/edit.card.reducer.js
@@ -0,0 +1,21 @@
+// reducer to hold single card object being edited
+const editEvent = (state = {}, action) => {
+    switch (action.type) {
+        case 'SET_EDIT_CARD':
+            return action.payload;
+        case 'EDIT_NAME':
+            return {...state, name: action.payload};
+        case 'EDIT_OCCASION_ID':
+            return {...state, occasion_id: action.payload};
+        case 'EDIT_CATEGORY_ID':
+            return {...state, category_id: action.payload};
+        case 'EDIT_ARTIST':
+            return {...state, artist: action.payload};
+        case 'EDIT_DETAILS':
+            return {...state, details: action.payload};            
+        default:
+            return state
+    }
+}; //end editEvent
+
+export default editEvent;
