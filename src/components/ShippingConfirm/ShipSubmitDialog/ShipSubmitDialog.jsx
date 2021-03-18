63d59b1d7684d3d2044a2d5745ad7d0c2a38d8c5
Michael Dunn <dunnmj42@gmail.com>
2021-03-18 15:43:30 -0500

    Shipping submit dialog data update and props

--- src/components/ShippingConfirm/ShipSubmitDialog/ShipSubmitDialog.jsx
+++ src/components/ShippingConfirm/ShipSubmitDialog/ShipSubmitDialog.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 // MUI
 import {
@@ -11,29 +11,38 @@ import {
   DialogContentText,
   DialogTitle,
   Slide,
-} from '@material-ui/core';
+} from "@material-ui/core";
 
 // Transition on mount
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
-export default function ShipSubmitDialog({ openSubmit, setOpenSubmit, personId, eventId }) {
+export default function ShipSubmitDialog({
+  openSubmit,
+  setOpenSubmit,
+  personId,
+  event,
+  eventId,
+}) {
   //hooks
   const dispatch = useDispatch();
   const history = useHistory();
-  const event = useSelector((store) => store.event);
+  // const event = useSelector((store) => store.event);
   const person = useSelector((store) => store.person);
 
   //click handlers
   const handleSubmit = () => {
     //close dialog
     setOpenSubmit(false);
-    dispatch({ type: 'UPDATE_SHIPPING', payload: { event, person, personId, eventId } });
+    dispatch({
+      type: "UPDATE_SHIPPING",
+      payload: { event, person, personId, eventId },
+    });
     //TODO: dispatch for an order (If we want one to show on admin page)
 
     //redirect to dashboard after order submitted
-    history.push('/dashboard');
+    history.push("/dashboard");
   };
 
   const handleBack = () => {
@@ -50,16 +59,19 @@ export default function ShipSubmitDialog({ openSubmit, setOpenSubmit, personId, 
         onClose={handleBack}
       >
         <DialogTitle id="alert-dialog-slide-title">
-          {'Confirm Order?'}
+          {"Confirm Order?"}
         </DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-slide-description">
-            Your card will be shipped to {event.ship_to_me ? <>YOU</>:<>person?.name at person?.address</>}
-            We will make sure it gets delivered on {event?.date}
+            Your card will be shipped to{" "}
+            {event.ship_to_me ? <>YOU</> : <>{event?.name} at {person?.address}</>}
+          </DialogContentText>
+          <DialogContentText>
+            We will make sure it gets delivered on {new Date(event?.date).toLocaleDateString('en-US')}
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleBack} color="inherit">
+          <Button onClick={handleBack} color="secondary">
             Back
           </Button>
           <Button onClick={handleSubmit} color="primary" variant="contained">
