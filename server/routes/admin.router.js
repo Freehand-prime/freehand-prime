e0f0495b5ac4370d8516167a514344f50d45571d
NateMacLaurin <blackmagenate@gmail.com>
2021-03-18 02:25:03 -0500

    removed unused routers from admin router

--- server/routes/admin.router.js
+++ server/routes/admin.router.js
@@ -104,36 +104,4 @@ router.delete('/card/:id', rejectUnauthenticated, (req, res) => {
         });
 });
 
-/**
- * adminOccasions routes for occasions
- */
-  
-router.post('/occasion', rejectUnauthenticated, (req, res) => {
-    // POST route code here
-});
-
-router.put('/occasion', rejectUnauthenticated, (req, res) => {
-    // PUT route code here
-});
-
-router.delete('/occasion/:id', rejectUnauthenticated, (req, res) => {
-    // DELETE route code here
-});
-
-/**
- * adminOccasions routes for categories
- */
-
-router.post('/category', rejectUnauthenticated, (req, res) => {
-    // POST route code here
-});
-
-router.put('/category', rejectUnauthenticated, (req, res) => {
-    // PUT route code here
-});
-
-router.delete('/category/:id', rejectUnauthenticated, (req, res) => {
-    // DELETE route code here
-});
-
 module.exports = router;
