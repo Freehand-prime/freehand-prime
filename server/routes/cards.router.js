e833e1487f130e45d8e9942dbdf9fd623b7ba0ba
Michael Dunn <dunnmj42@gmail.com>
2021-04-20 13:49:02 -0500

    unauth card router

--- server/routes/cards.router.js
+++ server/routes/cards.router.js
@@ -10,27 +10,22 @@ const {
  */
 
 // GET cards if authenticated
-router.get("/", rejectUnauthenticated, (req, res) => {
-  if (req.isAuthenticated()) {
-    // store query string in route scope
-    const query = `
+router.get("/", (req, res) => {
+  // store query string in route scope
+  const query = `
       SELECT * FROM "cards"
       ORDER BY "id" ASC`;
-    pool
-      .query(query)
-      .then((result) => {
-        // sends cards rows to client on successful pool query
-        res.send(result.rows);
-      })
-      .catch((error) => {
-        console.error(error);
-        // sends response 500 'Internal Server Error' on pool query error
-        res.sendStatus(500);
-      });
-  } else {
-    // send response 403 'Forbidden' if user is not authenticated
-    res.sendStatus(403);
-  }
+  pool
+    .query(query)
+    .then((result) => {
+      // sends cards rows to client on successful pool query
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.error(error);
+      // sends response 500 'Internal Server Error' on pool query error
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
